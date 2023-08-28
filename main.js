a13f5e1a3b090ade141ab980fe41cda60c94bd99
MVRaska <143351048+MVRaska@users.noreply.github.com>
2023-08-29 01:17:26 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+$(window).on('scroll', function() {
+    var scroll = $(window).scrollTop();
+    if(scroll >= 50) {
+        $('.sticky').addClass('stickyAdd');
+    } else {
+        $('.sticky').removeClass('stickyAdd');
+    }
+});
+
+
+//////////////////////////////////
+// alert('JS ready');
+const naslov = 'Full digitalna usluga';
+let i = 0;
+
+const kuckanje = () => {
+    if (i < naslov.length) {
+        document.querySelector('.tipkajuciNaslov').innerHTML += naslov.charAt(i);
+        i++;
+
+        setTimeout(kuckanje, 150);
+    }
+}
+kuckanje ();
